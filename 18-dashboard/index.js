b09d779317682d69667c00f3f0bb6294775d16cd
andrepavanelli7 <pavanellidev@gmail.com>
2023-03-19 13:15:53 -0300

    estudos 19/03/23

--- 18-dashboard/index.js
+++ 18-dashboard/index.js
@@ -26,5 +26,16 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     }
     
     setInterval(getTime, 5000)
+
+    navigator.geolocation.getCurrentPosition(position => {
+      fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data.weather[0].icon)
+          document.querySelector('.weather-icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`
+        })
+        .catch(err => console.error(err))
+    }) 
+
     
     
